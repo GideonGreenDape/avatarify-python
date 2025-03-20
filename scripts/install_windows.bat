150b3d3e5ce5ce62142f9e7156cce0ee2acbb9b5
Ufuoma Gideon Iboyi <ufuomaiboyi23@yahoo.com>
2025-03-20 15:32:14 +0100

    Update install_windows.bat
    fixed resolver issue

--- scripts/install_windows.bat
+++ scripts/install_windows.bat
@@ -6,15 +6,25 @@ REM call git --version >nul 2>&1 && ( echo git found ) || ( echo git not found. 
 
 call scripts/settings_windows.bat
 
+REM Create and activate Conda environment
 call conda create -y -n %CONDA_ENV_NAME% python=3.7
 call conda activate %CONDA_ENV_NAME%
 
-call conda install -y numpy==1.19.0 scikit-image python-blosc==1.7.0 -c conda-forge
-call conda install -y pytorch==1.7.1 torchvision cudatoolkit=11.0 -c pytorch
+REM Install dependencies
+call conda install -y numpy=1.19.0 scikit-image python-blosc=1.7.0 -c conda-forge
+
+REM Fix: Try installing PyTorch via Conda first, fallback to Pip if necessary
+call conda install -y pytorch=1.7.1 torchvision cudatoolkit=11.0 -c pytorch || (
+    echo PyTorch not found in Conda. Installing via Pip...
+    call pip install torch==1.7.1+cu110 torchvision==0.8.2+cu110 -f https://download.pytorch.org/whl/torch_stable.html
+)
+
 call conda install -y -c anaconda git
 
-REM ###FOMM###
+REM Clone First Order Motion Model (FOMM)
 call rmdir fomm /s /q
 call git clone https://github.com/alievk/first-order-model.git fomm
 
-call pip install -r requirements.txt --use-feature=2020-resolver
+REM Install Python dependencies (remove invalid --use-feature=2020-resolver flag)
+call pip install -r fomm/requirements.txt
+
